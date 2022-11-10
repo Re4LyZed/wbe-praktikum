a558d41a4f0ae0f1accbc8a1451532b2b902ff61
Roman Gerber <gerbero5@students.zhaw.ch>
2022-11-10 16:15:29 +0100

    lab 08 t2

--- src/08/code/t2/connect4.js
+++ src/08/code/t2/connect4.js
@@ -1,3 +1,11 @@
+const BOARD_ROW = 6
+const BOARD_COL = 7
+const BOARD_TOT = BOARD_ROW * BOARD_COL
+
+const PIECE_BLANK = ''
+const PIECE_BLUE = 'blue piece'
+const PIECE_RED = 'red piece'
+
 function elt(type, attrs, ...children) {
 
     let node = document.createElement(type)
@@ -14,34 +22,76 @@ function elt(type, attrs, ...children) {
     return node
 }
 
-const getRandomInt = function (min, max) {
+function getRandomInt (min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
 }
 
+function initBoard (board) {
 
-const initBoard = function (board) {
-
-    for (let index = 0; index < 42; index++) {
+    for (let index = 0; index < BOARD_TOT; index++) {
         board.appendChild(elt("div", { class: "field" }))
     }
 }
 
-const fillRandom = function (fieldList) {
+function fillRandom (state) {
 
-    for (let index = 0; index < 42; index++) {
+    let pieceRow = getRandomInt(0, BOARD_ROW - 1)
+    let pieceCol = getRandomInt(0, BOARD_COL - 1)
 
-        // -1 = none ; 0 = red ; 1 = blue
-        let random = getRandomInt(-1, 2)
+    // -1 = blank ; 0 = red ; 1 = blue
+    let randomAction = getRandomInt(-1, 2)
 
-        if (random >= 0) {
-            fieldList[index].appendChild(elt("div", { class: random == 0 ? "red piece" : "blue piece" }))
+    let pieceValue
+
+    switch (randomAction) {
+        case -1:
+            pieceValue = PIECE_BLANK
+            break;
+        case 0:
+            pieceValue = PIECE_RED
+            break;
+        case 1:
+            pieceValue = PIECE_BLUE
+            break;
+    }
+
+    state[pieceRow][pieceCol] = pieceValue
+}
+
+function setBoard (fieldList, state) {
+
+    for (let row = 0; row < BOARD_ROW; row++) {
+        for (let col = 0; col < BOARD_COL; col++) {
+
+            let field = fieldList[row * BOARD_COL + col]
+
+            if (field.children.length > 0) {
+                // update piece
+                field.children[0].setAttribute("class", state[row][col])
+            } else {
+                // add piece
+                field.appendChild(elt("div", { class: state[row][col] }))
+            }
         }
     }
 }
 
+function playGame (fieldList, state) {
+
+    setInterval(() => {
+    
+        fillRandom(state)
+
+        setBoard(fieldList, state)
+
+    }, 1000);
+}
+
 const showBoard = function () {
+
+    let state = Array(BOARD_ROW).fill(PIECE_BLANK).map(el => Array(BOARD_COL).fill(PIECE_BLANK))
 
     let board = document.getElementsByClassName("board")[0]
 
@@ -49,9 +99,7 @@ const showBoard = function () {
 
     let fieldList = document.getElementsByClassName("field")
 
-    fillRandom(fieldList)
-
-
+    playGame(fieldList, state)
 
     /*
     <div class="board">
