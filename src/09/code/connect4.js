4af3c3c33db79915986137f80c62d025b1bb65f9
Roman Gerber <gerbero5@students.zhaw.ch>
2022-11-10 18:02:16 +0100

    lab 09

--- src/09/code/connect4.js
+++ src/09/code/connect4.js
@@ -2,9 +2,10 @@ const BOARD_ROW = 6
 const BOARD_COL = 7
 const BOARD_TOT = BOARD_ROW * BOARD_COL
 
-const PIECE_BLANK = ''
-const PIECE_BLUE = 'blue piece'
-const PIECE_RED = 'red piece'
+const PIECE = 'piece'
+const BLANK = ''
+const BLUE = 'blue'
+const RED = 'red'
 
 function elt(type, attrs, ...children) {
 
@@ -22,42 +23,54 @@ function elt(type, attrs, ...children) {
     return node
 }
 
-function getRandomInt (min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-}
-
 function initBoard (board) {
+
+    board.replaceChildren([])
 
     for (let index = 0; index < BOARD_TOT; index++) {
         board.appendChild(elt("div", { class: "field" }))
     }
 }
 
-function fillRandom (state) {
+function setState (state, fieldListIndex) {
 
-    let pieceRow = getRandomInt(0, BOARD_ROW - 1)
-    let pieceCol = getRandomInt(0, BOARD_COL - 1)
+    // read col
+    let col = fieldListIndex
 
-    // -1 = blank ; 0 = red ; 1 = blue
-    let randomAction = getRandomInt(-1, 2)
+    while (col >= BOARD_COL) {
+        col -= BOARD_COL
+    }
+    
+    // determine row
+    let row = -1
+    for (let row_index = BOARD_ROW - 1; row_index >= 0; row_index--) {
+        if (state.board[row_index][col] == BLANK) {
+            row = row_index
+            break
+        }
+    }
 
+    if (row == -1) {
+        return false
+    }
+    
+    // set player value
     let pieceValue
 
-    switch (randomAction) {
-        case -1:
-            pieceValue = PIECE_BLANK
+    switch (state.nextPlayerTurn) {
+        case RED:
+            pieceValue = PIECE.concat(' ', RED)
+            state.nextPlayerTurn = BLUE
             break;
-        case 0:
-            pieceValue = PIECE_RED
-            break;
-        case 1:
-            pieceValue = PIECE_BLUE
+        case BLUE:
+            pieceValue = PIECE.concat(' ', BLUE)
+            state.nextPlayerTurn = RED
             break;
     }
 
-    state[pieceRow][pieceCol] = pieceValue
+    state.board[row][col] = pieceValue
+    
+    return true
 }
 
 function setBoard (fieldList, state) {
@@ -69,37 +82,58 @@ function setBoard (fieldList, state) {
 
             if (field.children.length > 0) {
                 // update piece
-                field.children[0].setAttribute("class", state[row][col])
+                field.children[0].setAttribute("class", state.board[row][col])
             } else {
                 // add piece
-                field.appendChild(elt("div", { class: state[row][col] }))
+                field.appendChild(elt("div", { class: state.board[row][col] }))
             }
         }
     }
 }
 
-function playGame (fieldList, state) {
-
-    setInterval(() => {
+function playGame (fieldList, state, fieldListIndex) {
     
-        fillRandom(state)
+    success = setState(state, fieldListIndex)
+
+    if (success) {
 
         setBoard(fieldList, state)
+        
+        document.getElementById("nextPlayerTurn").textContent = state.nextPlayerTurn
 
-    }, 1000);
+    }
 }
 
 const showBoard = function () {
 
-    let state = Array(BOARD_ROW).fill(PIECE_BLANK).map(el => Array(BOARD_COL).fill(PIECE_BLANK))
+    // init
+    let state = {
+        board: Array(BOARD_ROW).fill(BLANK).map(el => Array(BOARD_COL).fill(BLANK)),
+        nextPlayerTurn: BLUE
+    }
 
+    // nextPlayerTurn
+    document.getElementById("nextPlayerTurn").textContent = state.nextPlayerTurn
+
+    // board
     let board = document.getElementsByClassName("board")[0]
 
     initBoard(board)
 
+    // fieldList
     let fieldList = document.getElementsByClassName("field")
 
-    playGame(fieldList, state)
+    // event: click - fieldList
+    Array.from(document.getElementsByClassName("field")).forEach(element => {
+        element.addEventListener("click", (event) => {
+            playGame(fieldList, state, Array.from(fieldList).indexOf(event.target))
+        })
+    })
+
+    // event: click - newGame
+    document.getElementById("newGame").addEventListener("click", () => {
+        showBoard()
+    })
 
     /*
     <div class="board">
