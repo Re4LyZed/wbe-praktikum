948b2d2aec91cf18ec4e869f6832ed4756cf4a66
Roman Gerber <gerbero5@students.zhaw.ch>
2022-11-24 14:08:47 +0100

    lab 11 t1

---
+++ src/11/code/t1/render-sjdon.js
@@ -0,0 +1,58 @@
+function deserialize(element) {
+
+    let type = element[0]
+    let value = ""
+    let attrs = []
+    let children = []
+
+    Array.from(element).forEach(element => {
+        if (Array.isArray(element)) {
+            children.push(element)
+        } else if (typeof element === 'object') {
+            attrs = element
+        } else if (element != type) {
+            value = element
+        }
+    })
+
+    return { type: type, value: value, attrs: attrs, children: children }
+}
+
+function renderSJDON(element, appRoot) {
+    appRoot.appendChild(elt(element))
+}
+
+function elt(element) {
+
+    let eleObj = deserialize(element)
+
+    let node = document.createElement(eleObj.type)
+
+    node.innerHTML = eleObj.value
+
+    for (a in eleObj.attrs) {
+        node.setAttribute(a, eleObj.attrs[a])
+    }
+
+    for (let child of eleObj.children) {
+        node.appendChild(elt(child))
+    }
+
+    return node
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    const element =
+        ["div", { style: "background: salmon" },
+            ["h1", "Hello World"],
+            ["h2", { style: "text-align:right", id: "funh2" }, "from our library"],
+            ["h3", "untertitel", { style: "text-align:right", id: "funh3" }],
+            ["ul", ["li", "eins"], ["li", "zwei"]]]
+
+    let appRoot = document.getElementById("app")
+
+    renderSJDON(element, appRoot)
+})
+
+module.exports = { renderSJDON }
